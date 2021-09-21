0cb340345b033308dc86de428a1417b6dbc3a4b2
sadath <sadathshariff65@gmail.com>
2021-09-21 17:57:27 +0530

    Completed The Weather App

---
+++ app.js
@@ -0,0 +1,36 @@
+let inputCityName = document.querySelector(".inputCity");
+let btn = document.querySelector(".inputBtn");
+let cityName = document.querySelector(".cityName");
+let temp = document.querySelector(".temperature");
+let desc = document.querySelector(".description");
+let cel = document.querySelector(".tempInCelsius");
+
+function clickHandler() {
+  var inputValue = inputCityName.value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=60d763a183e9cd9fe4c26b43d74c699c`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var nameValue = data["name"];
+      var temperature = data["main"]["temp"] + " K";
+      var kelvinTemp = data["main"]["temp"];
+      var description = data["weather"][0]["description"];
+
+      cityName.innerText = nameValue;
+      temp.innerText = temperature;
+      cel.innerText = kelvinToCelsius(kelvinTemp);
+
+      desc.innerText = description;
+    })
+    .catch((err) => alert(`${inputValue} Wrong City Name`));
+}
+
+function kelvinToCelsius(temperature) {
+  var kelToCel = (temperature - 273.15).toFixed(2) + "°C";
+  return kelToCel;
+}
+
+console.log(kelvinToCelsius(293));
+
+btn.addEventListener("click", clickHandler);
